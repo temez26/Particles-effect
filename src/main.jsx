344228507951cda183a7a07b4eff1947e6e9a80a
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2023-09-10 12:15:46 +0300

    missed folder

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Background from './App.jsx'
+
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   
+    <Background/>
+    
+  </React.StrictMode>,
+)
